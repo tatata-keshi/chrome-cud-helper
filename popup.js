1d2557b202b040cdf5d3065555189cdda9bd8ee1
tatata-keshi <takeshiinoue0718@gmail.com>
2024-11-10 20:38:55 +0900

    mod: popup改良

--- popup.js
+++ popup.js
@@ -1,3 +1,57 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const radioButtons = document.querySelectorAll("input[name='filter']");
+
+    // 前回の選択を復元
+    chrome.storage.sync.get("selectedFilter", (data) => {
+        if (data.selectedFilter) {
+            const selectedRadio = document.querySelector(`input[name="filter"][value="${data.selectedFilter}"]`);
+            if (selectedRadio) {
+                selectedRadio.checked = true;
+            }
+        }
+    });
+
+    radioButtons.forEach((radio) => {
+        radio.addEventListener("change", async (event) => {
+            let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+            const selectedFilter = event.target.value;
+
+            chrome.storage.sync.set({ selectedFilter: selectedFilter });
+
+            chrome.scripting.executeScript({
+                target: { tabId: tab.id },
+                func: applyFilter,
+                args: [selectedFilter]
+            });
+        });
+    });
+});
+
+/**
+ * SVGフィルターを適用する
+ *
+ * @param filterId
+ */
+function applyFilter(filterId = "none") {
+    const overlay = document.getElementById("cvdCustomOverlay");
+    if (filterId === "none") {
+        document.body.style.filter = "none";
+        if (overlay) {
+            document.querySelectorAll(".cvd-color-box-filtered").forEach((box) => {
+                box.style.filter = "none";
+            });
+        }
+    } else {
+        document.body.style.filter = `url(#${filterId})`;
+        if (overlay) {
+            document.querySelectorAll(".cvd-color-box-filtered").forEach((box) => {
+                box.style.filter = `url(#${filterId})`;
+            });
+        }
+    }
+}
+
 document.getElementById("showOverlay").addEventListener("click", () => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         chrome.scripting.executeScript({
@@ -14,201 +68,190 @@ function showOverlayInContentScript() {
     if (document.getElementById("customOverlay")) return; // 重複表示防止
 
     const overlay = document.createElement("div");
-    overlay.id = "customOverlay";
+    overlay.id = "cvdCustomOverlay";
     overlay.innerHTML = `
-    <div class="overlay-content">
-      <button id="exitOverlay">Exit</button>
-      <button id="colorPickerMode">Pick Element Color</button>
-      <div class="color-display" id="colorDisplay">
-        <div class="color-box" id="colorBox1">
-          <div class="color-shape" id="shape1"></div>
-          <div class="color-controls">
-            R: <input type="range" min="0" max="255" id="sliderR1"><br>
-            G: <input type="range" min="0" max="255" id="sliderG1"><br>
-            B: <input type="range" min="0" max="255" id="sliderB1">
-          </div>
+    <div class="cvd-exit-overlay-button-container">
+        <button class="cvd-exit-overlay-button">Exit</button>        
+    </div>
+    <div class="cvd-color-display-container">
+        <h2>Color1</h2>
+        <div class="cvd-color-display">
+            <div id="boxColor1" class="cvd-color-box"></div>
+            <div id="boxColorFiltered1" class="cvd-color-box cvd-color-box-filtered"></div>
+            <div class="cvd-color-controls">
+                <div class="cvd-color-slider">
+                    <p>R: </p>
+                    <input type="range" min="0" max="255" id="sliderColorR1">
+                    <p id="sliderColorR1Value"></p>
+                </div>
+                <div class="cvd-color-slider">
+                    <p>G: </p>
+                    <input type="range" min="0" max="255" id="sliderColorG1">
+                    <p id="sliderColorG1Value"></p>
+                </div>
+                <div class="cvd-color-slider">
+                    <p>B: </p>
+                    <input type="range" min="0" max="255" id="sliderColorB1">
+                    <p id="sliderColorB1Value"></p>
+                </div>
+            </div>
         </div>
-        <div class="color-box" id="colorBox2">
-          <div class="color-shape" id="shape2"></div>
-          <div class="color-controls">
-            R: <input type="range" min="0" max="255" id="sliderR2"><br>
-            G: <input type="range" min="0" max="255" id="sliderG2"><br>
-            B: <input type="range" min="0" max="255" id="sliderB2">
-          </div>
+        <div class="cvd-color-display">
+            <div id="boxBackground1" class="cvd-color-box"></div>
+            <div id="boxBackgroundFiltered1" class="cvd-color-box cvd-color-box-filtered"></div>
+            <div class="cvd-color-controls">
+                <div class="cvd-color-slider">
+                    <p>R: </p>
+                    <input type="range" min="0" max="255" id="sliderBackgroundR1">
+                    <p id="sliderBackgroundR1Value"></p>
+                </div>
+                <div class="cvd-color-slider">
+                    <p>G: </p>
+                    <input type="range" min="0" max="255" id="sliderBackgroundG1">
+                    <p id="sliderBackgroundG1Value"></p>
+                </div>
+                <div class="cvd-color-slider">
+                    <p>B: </p>
+                    <input type="range" min="0" max="255" id="sliderBackgroundB1">
+                    <p id="sliderBackgroundB1Value"></p>
+                </div>
+            </div>
         </div>
-      </div>
-      <div class="filters">
-        <label><input type="radio" name="filter" value="protanopia"> Protanopia</label><br>
-        <label><input type="radio" name="filter" value="deuteranopia"> Deuteranopia</label><br>
-        <label><input type="radio" name="filter" value="tritanopia"> Tritanopia</label><br>
-      </div>
-      <button id="resetFilter">Reset</button>
+        <h2>Color2</h2>
+        <div class="cvd-color-display">
+            <div id="boxColor2" class="cvd-color-box"></div>
+            <div id="boxColorFiltered2" class="cvd-color-box cvd-color-box-filtered"></div>
+            <div class="cvd-color-controls">
+                <div class="cvd-color-slider">
+                    <p>R: </p>
+                    <input type="range" min="0" max="255" id="sliderColorR2">
+                    <p id="sliderColorR2Value"></p>
+                </div>
+                <div class="cvd-color-slider">
+                    <p>G: </p>
+                    <input type="range" min="0" max="255" id="sliderColorG2">
+                    <p id="sliderColorG2Value"></p>
+                </div>
+                <div class="cvd-color-slider">
+                    <p>B: </p>
+                    <input type="range" min="0" max="255" id="sliderColorB2">
+                    <p id="sliderColorB2Value"></p>
+                </div>
+            </div>
+        </div>
+        <div class="cvd-color-display">
+            <div id="boxBackground2" class="cvd-color-box"></div>
+            <div id="boxBackgroundFiltered2" class="cvd-color-box cvd-color-box-filtered"></div>
+            <div class="cvd-color-controls">
+                <div class="cvd-color-slider">
+                    <p>R: </p>
+                    <input type="range" min="0" max="255" id="sliderBackgroundR2">
+                    <p id="sliderBackgroundR2Value"></p>
+                </div>
+                <div class="cvd-color-slider">
+                    <p>G: </p>
+                    <input type="range" min="0" max="255" id="sliderBackgroundG2">
+                    <p id="sliderBackgroundG2Value"></p>
+                </div>
+                <div class="cvd-color-slider">
+                    <p>B: </p>
+                    <input type="range" min="0" max="255" id="sliderBackgroundB2">
+                    <p id="sliderBackgroundB2Value"></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="cvd-toggle-color-pick-mode-button-container">
+        <button class="cvd-toggle-color-pick-mode-button" id="colorPickerMode">Pick Element Color</button>
     </div>
   `;
-    document.body.appendChild(overlay);
+    document.documentElement.appendChild(overlay);
 
     // スタイルをインジェクト
     const style = document.createElement("style");
     style.textContent = `
-    #customOverlay {
-      position: fixed;
-      top: 0;
-      right: 0;
-      width: 350px;
-      height: 400px;
-      background-color: rgba(0, 0, 0, 0.8);
-      color: white;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      padding: 10px;
-      z-index: 10000;
+    #cvdCustomOverlay {
+        position: fixed;
+        top: 0;
+        right: 0;
+        width: 320px;
+        height: 440px;
+        background-color: white;
+        color: black;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 8px 12px;
+        z-index: 10000;
+        border: 1px solid #ccc;
+        border-radius: 4px;
     }
-    .overlay-content {
-      width: 100%;
-      position: relative;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
+    #cvdCustomOverlay h2 {
+        font-size: 16px;
+        font-weight: bold;
     }
-    #exitOverlay {
-      position: absolute;
-      top: 5px;
-      right: 5px;
-      background-color: #ff4444;
-      border: none;
-      color: white;
-      padding: 3px 6px;
-      cursor: pointer;
+    .cvd-exit-overlay-button-container {
+        width: 100%;
+        display: flex;
+        justify-content: flex-end;
     }
-    #colorPickerMode {
-      margin-top: 10px;
-      background-color: #4CAF50;
-      border: none;
-      color: white;
-      padding: 5px 10px;
-      cursor: pointer;
+    .cvd-exit-overlay-button {
+        background-color: red;
+        border: none;
+        border-radius: 4px;
+        color: white;
+        padding: 4px 8px;
+        cursor: pointer;
     }
-    .color-display {
-      display: flex;
-      justify-content: space-around;
-      width: 100%;
-      margin-top: 10px;
+    .cvd-color-display-container {
+        display: flex;
+        flex-direction: column;
+        gap: 8px;
+        width: 100%;
     }
-    .color-box {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      width: 120px;
+    .cvd-color-display {
+        display: flex;
+        gap: 8px;
+        width: 100%;
+        align-items: center;
+        margin-bottom: 8px;
     }
-    .color-shape {
-      width: 50px;
-      height: 50px;
-      margin-bottom: 10px;
-      background-color: gray;
+    .cvd-color-box {
+        width: 48px;
+        height: 48px;
+        background-color: gray;
+        border: 1px solid #ccc;
+        border-radius: 4px;
     }
-    .color-controls input {
+    .cvd-color-controls {
+        display: flex;
+        flex-direction: column;
+        gap: 4px;
+    }
+    .cvd-color-controls input {
       width: 80px;
-      margin-bottom: 5px;
     }
-    .filters {
-      margin-top: 20px;
+    .cvd-color-slider {
+        display: flex;
+        align-items: center;
+        gap: 4px;
     }
-    #resetFilter {
-      margin-top: auto;
-      background-color: #4444ff;
-      border: none;
-      color: white;
-      padding: 5px 10px;
-      cursor: pointer;
+    .cvd-toggle-color-pick-mode-button-container {
+        margin-top: 16px;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+    }
+    .cvd-toggle-color-pick-mode-button {
+        background-color: blue;
+        border: none;
+        border-radius: 4px;
+        color: white;
+        padding: 4px 8px;
+        cursor: pointer;
     }
   `;
     document.head.appendChild(style);
-
-    // SVGフィルターをインジェクト
-    const svgFilters = document.createElement("div");
-    svgFilters.innerHTML = `
-    <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
-      <!-- Protanopia Filter -->
-      <filter id="protanopia" color-interpolation-filters="linearRGB">
-        <feColorMatrix type="matrix" in="SourceGraphic" values="
-            0.10889,0.89111,-0.00000,0,0
-            0.10889,0.89111,0.00000,0,0
-            0.00447,-0.00447,1.00000,0,0
-            0,0,0,1,0"
-        />
-      </filter>
-      <!-- Deuteranopia Filter -->
-      <filter id="deuteranopia" color-interpolation-filters="linearRGB">
-        <feColorMatrix type="matrix" in="SourceGraphic" values="
-            0.29031,0.70969,-0.00000,0,0
-            0.29031,0.70969,-0.00000,0,0
-            -0.02197,0.02197,1.00000,0,0
-            0,0,0,1,0"
-        />
-      </filter>
-      <!-- Tritanopia Filter -->
-      <filter id="tritanopia" color-interpolation-filters="linearRGB">
-        <!-- 
-            Projection 1, with a special alpha that encodes the separation plane.
-            If dot(rgb, n) > 0, then use projection 1, otherwise use projection 2.
-            This is encoded in alpha by:
-                - Applying a 1.0 factor on the source alpha so that 0 input alpha remains 0
-                - Subtracting 0.2 so that negative values become < 0.8 and position values >= 0.8
-                - It is important to normalize the factors to keep a good numerical accuracy
-                  and to keep a large alpha threshold since the RGB values are then stored
-                  premultiplied by alpha.
-                - This assumes that negative values get clipped to 0, and positive
-                  values clipped to 1, without overflowing, etc. Which seems to be the case
-                  on all browsers.
-          -->
-        <feColorMatrix type="matrix" in="SourceGraphic" result="ProjectionOnPlane1" values="
-            1.01354, 0.14268, -0.15622, 0, 0
-            -0.01181, 0.87561, 0.13619, 0, 0
-            0.07707, 0.81208, 0.11085, 0, 0
-            7.92482, -5.66475, -2.26007, 1, -0.2"
-        />
-        <!-- 
-            Binarize alpha. 5 values means the last chunk will start at 0.8.
-            All the values below 0.8 will become 0 (correspond to the dot
-            product with the separation plane being negative) and above will become 1
-        -->        
-        <feComponentTransfer in="ProjectionOnPlane1" result="ProjectionOnPlane1">
-            <feFuncA type="discrete" tableValues="0 0 0 0 1"/>
-        </feComponentTransfer>
-
-        <feColorMatrix type="matrix" in="SourceGraphic" result="ProjectionOnPlane2" values="
-            0.93337, 0.19999, -0.13336, 0, 0
-            0.05809, 0.82565, 0.11626, 0, 0
-            -0.37923, 1.13825, 0.24098, 0, 0
-            0,0,0,1,0"
-        />
-
-        <!-- Uncomment the debug black matrix to see which pixels go to which plane -->
-        <!-- <feColorMatrix type="matrix" in="SourceGraphic" result="ProjectionOnPlane2" values="0,0,0,0,0 0,0,0,0,0 0,0,0,0,0 0,0,0,1,0"/> -->
-
-        <!-- Blend the two projections, picking one or the other depending on alpha. -->
-        <feBlend in="ProjectionOnPlane1" in2="ProjectionOnPlane2" mode="normal"/>
-    </filter>
-    </svg>
-  `;
-    document.body.appendChild(svgFilters);
-
-    // フィルター適用機能
-    document.querySelectorAll("input[name='filter']").forEach((radio) => {
-        radio.addEventListener("change", (event) => {
-            const selectedFilter = event.target.value;
-            document.body.style.filter = `url(#${selectedFilter})`;
-        });
-    });
-
-    // リセットボタンでフィルター解除＆ラジオボタンの選択をリセット
-    document.getElementById("resetFilter").addEventListener("click", () => {
-        document.body.style.filter = "none";
-        document.querySelectorAll("input[name='filter']").forEach((radio) => {
-            radio.checked = false; // ラジオボタンの選択を解除
-        });
-    });
 
     // 色の初期値を保持するオブジェクト
     const selectedColors = [
@@ -216,18 +259,32 @@ function showOverlayInContentScript() {
         { r: 128, g: 128, b: 128 }
     ];
 
-    // RGBスライダーのイベントリスナー
+    const selectedBackgrounds = [
+        { r: 128, g: 128, b: 128 },
+        { r: 128, g: 128, b: 128 }
+    ]
+
     function updateColorFromSliders(index) {
         const color = selectedColors[index];
-        document.getElementById(`shape${index + 1}`).style.backgroundColor = `rgb(${color.r}, ${color.g}, ${color.b})`;
+        document.getElementById(`boxColor${index + 1}`).style.backgroundColor = `rgb(${color.r}, ${color.g}, ${color.b})`;
+        document.getElementById(`boxColorFiltered${index + 1}`).style.backgroundColor = `rgb(${color.r}, ${color.g}, ${color.b})`;
+    }
+
+    function updateBackgroundFromSliders(index) {
+        const background = selectedBackgrounds[index];
+        document.getElementById(`boxBackground${index + 1}`).style.backgroundColor = `rgb(${background.r}, ${background.g}, ${background.b})`;
+        document.getElementById(`boxBackgroundFiltered${index + 1}`).style.backgroundColor = `rgb(${background.r}, ${background.g}, ${background.b})`;
     }
 
     ["R", "G", "B"].forEach((channel) => {
         for (let i = 1; i <= 2; i++) {
-            document.getElementById(`slider${channel}${i}`).addEventListener("input", (e) => {
-                const value = parseInt(e.target.value);
-                selectedColors[i - 1][channel.toLowerCase()] = value;
+            document.getElementById(`sliderColor${channel}${i}`).addEventListener("input", (e) => {
+                selectedColors[i - 1][channel.toLowerCase()] = parseInt(e.target.value);
                 updateColorFromSliders(i - 1);
+            });
+            document.getElementById(`sliderBackground${channel}${i}`).addEventListener("input", (e) => {
+                selectedBackgrounds[i - 1][channel.toLowerCase()] = parseInt(e.target.value);
+                updateBackgroundFromSliders(i - 1);
             });
         }
     });
@@ -264,7 +321,5 @@ function showOverlayInContentScript() {
         e.stopPropagation(); // イベントが伝播して消えないようにする
         overlay.remove();
         style.remove();  // スタイルも削除
-        svgFilters.remove();  // SVGフィルターも削除
-        document.body.style.filter = "none"; // フィルター解除
     });
 }
